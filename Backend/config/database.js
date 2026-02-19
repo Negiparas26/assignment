e7121b8a9a9de549be5cdb902843e31b0e2bbcde
Paras negi <89247131+Negiparas26@users.noreply.github.com>
2026-02-19 15:38:21 +0530

    Updated database config for Render

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,16 +1,52 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    port: process.env.DB_PORT,
-    logging: false, 
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+//     port: process.env.DB_PORT,
+//     logging: false, 
+//   }
+// );
+
+// module.exports = sequelize;
+
+
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+let sequelize;
+
+if (process.env.NODE_ENV === "development") {
+  // Render Production DB
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else {
+  // Local Development DB
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: "postgres",
+      port: process.env.DB_PORT,
+      logging: false,
+    }
+  );
+}
 
 module.exports = sequelize;
